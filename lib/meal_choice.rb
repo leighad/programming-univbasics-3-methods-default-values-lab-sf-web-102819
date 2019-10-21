7d8de7efda1d37b5047377c2962d4f67ac9cfd75
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-10-21 18:18:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@ def meal_choice (veg1, veg2, protein = 'meat')
   puts meal_statement
   meal_selection = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal_selection
-  meal_statement + meal_statement
+  meal_statement + meal_selection
 end
 
 
